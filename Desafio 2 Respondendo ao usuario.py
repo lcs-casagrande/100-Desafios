dbdae9e743102354fd61e4e83af96f8569c6a655
Lcasagrande <104326367+Usuwawe@users.noreply.github.com>
2022-07-24 09:25:10 -0300

    Add files via upload

---
+++ Desafio 2 Respondendo ao usuario.py
@@ -0,0 +1,5 @@
+dia=input('Qual dia do seu aniversário?')
+mes=input('Qual o mês do seu aniversário?')
+ano=input('Em qual ano você nasceu?')
+info=('Você nasceu no dia')
+print(info,dia,'/',mes,'/',ano)

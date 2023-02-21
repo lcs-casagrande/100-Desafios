d8490f0ad141d16dab0457db4fe5e89da58a5ef7
Lcasagrande <104326367+lcs-casagrande@users.noreply.github.com>
2023-02-21 15:55:48 -0300

    Add files via upload

---
+++ Desafio 97 Print def.py
@@ -0,0 +1,10 @@
+def escreva(tx):
+    tam = len(tx)
+    print('~' * 2 * tam)
+    print(f'  {tx.upper()}  ')
+    print('~' * 2 * tam)
+
+
+escreva('lucas')
+escreva('curso em video')
+escreva('fera')

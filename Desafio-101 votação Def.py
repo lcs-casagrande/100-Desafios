3d1d33200af7561b758f378786c5779c67ad4d7f
Lcasagrande <104326367+lcs-casagrande@users.noreply.github.com>
2023-02-26 12:34:00 -0300

    Add files via upload

---
+++ Desafio-101 votação Def.py
@@ -0,0 +1,15 @@
+from datetime import date
+def voto(idade):
+    print('--' * 50)
+    if idade < 16:
+        sit='não vota'
+    elif 17< idade < 70:
+        sit='o voto é obrigatório'
+    else:
+        sit='o voto é opcional'
+    return sit
+
+nasc=int(input('Em que ano você nasceu? '))
+atual = date.today().year
+idade = atual - nasc
+print(f'Com {idade} anos: {voto(idade)}.')
